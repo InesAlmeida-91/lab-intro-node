d371ec2fb7f05389d873f00db9fd109a955c33e5
Inês Almeida <ines_almeida_203@hotmail.com>
2023-05-17 23:39:24 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){return a-b}); 
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < 0 || pos >= this.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
   sum() {}
 
